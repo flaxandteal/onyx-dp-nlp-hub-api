979791d68c7eef20dbc24b5632c6593d00f77471
kamen.dimitrov1 <kamen.flaxandteal.co.uk>
2023-03-14 21:35:52 +0200

    started each request in a separate go routine with channels for the errors

--- api/hub.go
+++ api/hub.go
@@ -3,7 +3,9 @@ package api
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/ONSdigital/dp-nlp-hub/config"
 	"github.com/ONSdigital/dp-nlp-hub/models"
@@ -18,22 +20,53 @@ func HubHandler(cfg *config.Config) http.HandlerFunc {
 
 		var result models.Hub
 
-		// Gets the scrubber response
-		err := MakeRequest(r.Context(), cfg.ScrubberBase, models.GetScrubberParams(r.URL.Query()), &result.Scrubber)
-		if err != nil {
-			log.Warn(ctx, err.Error())
+		scrubberCh := make(chan error)
+		berlinCh := make(chan error)
+		categoryCh := make(chan error)
+
+		var wg sync.WaitGroup
+		wg.Add(3)
+
+		go func() {
+			defer wg.Done()
+			err := MakeRequest(r.Context(), cfg.ScrubberBase, models.GetScrubberParams(r.URL.Query()), &result.Scrubber)
+			scrubberCh <- err
+		}()
+
+		go func() {
+			defer wg.Done()
+			err := MakeRequest(r.Context(), cfg.BerlinBase, models.GetBerlinParams(r.URL.Query()), &result.Berlin)
+			berlinCh <- err
+		}()
+
+		go func() {
+			defer wg.Done()
+			err := MakeRequest(r.Context(), cfg.CategoryBase, models.GetCategoryParams(r.URL.Query()), &result.Category)
+			categoryCh <- err
+		}()
+
+		wg.Wait()
+		close(scrubberCh)
+		close(berlinCh)
+		close(categoryCh)
+
+		// Check for errors in each channel
+		for err := range scrubberCh {
+			if err != nil {
+				log.Warn(ctx, fmt.Sprintf("Scrubber error: %s", err.Error()))
+			}
 		}
 
-		// Gets the berlin response using a filter from url params and a query from scrubber
-		err = MakeRequest(r.Context(), cfg.BerlinBase, models.GetBerlinParams(r.URL.Query()), &result.Berlin)
-		if err != nil {
-			log.Warn(ctx, err.Error())
+		for err := range berlinCh {
+			if err != nil {
+				log.Warn(ctx, fmt.Sprintf("Berlin error: %s", err.Error()))
+			}
 		}
 
-		// Gets the category response using berlin normalized query
-		err = MakeRequest(r.Context(), cfg.CategoryBase, models.GetCategoryParams(r.URL.Query()), &result.Category)
-		if err != nil {
-			log.Warn(ctx, err.Error())
+		for err := range categoryCh {
+			if err != nil {
+				log.Warn(ctx, fmt.Sprintf("Category error: %s", err.Error()))
+			}
 		}
 
 		if err := json.NewEncoder(w).Encode(result); err != nil {
